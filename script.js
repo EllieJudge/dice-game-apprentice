205834779bd9659576074551bfc9468117b2d5b7
EllieJudge <judge_ellie@hotmail.com>
2019-08-02 16:58:03 +0100

    completed DOM stuff, now need css

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
 const rollButton = document.getElementById("rollButton");
-
-
-let score = 0;
+let score = parseInt(document.getElementById("score").textContent);
+let startAgain = document.getElementById("start-again");
+console.log(score)
 let number;
 
 
 rollButton.addEventListener("click", () => {
     function roll(){
-   
         number = Math.ceil((Math.random() * 6))
+        document.getElementById("image").src = `img/dice${number}.png`
         console.log(`rolled = ${number}`) 
     
-            if (number == 1) {
-                return console.log("Loser") 
-    
-            } else if (score >= 20) { 
-                return console.log("Winner!")
+            if (score >= 20) { 
+                console.log("Winner!")
+                document.getElementById("start-again").innerHTML = "Play again?"
+                document.getElementById("player1").innerHTML = "Winner!"
             }
+            else if  (number == 1) {
+                console.log("Loser") 
+                document.getElementById("start-again").innerHTML = "Start again?"
+                document.getElementById("player1").innerHTML = "Loser!"
+            } 
             else {
                 calcScore() 
             }
@@ -24,12 +28,14 @@ rollButton.addEventListener("click", () => {
     roll()
 
     function calcScore() {
-        score += number; 
-            console.log(`score = ${score}`) 
+        let showScore = score += number; 
+            document.getElementById("score").innerHTML = showScore;
+            console.log(`score = ${score}`);
     }
 
 });
 
 
-
-
+startAgain.addEventListener("click", () => {
+    location.reload()
+});
